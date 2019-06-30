eb69b1d5cc803a28e0b5ee878e8f73282fca1f75
BORIS552 <boris.biswas@gmail.com>
2019-06-30 11:09:27 +0530

    Camera Update

--- app_1/src/views/CameraView.vue
+++ app_1/src/views/CameraView.vue
@@ -1,11 +1,15 @@
 <template>
    <div class="camera-modal">
-        <video ref="video" class="camera-stream"/>
+        <video v-show="captureStatus" ref="video" class="camera-stream"/>
+        <canvas v-show="captureStatus" ref="canvas" id="canvas" width="640" height="480"><p>Testing</p></canvas>
+        <img v-show="captureStatus" v-bind:src="captureData" height="50" />
          <div class="camera-modal-container">
             <span @click="capture" class="take-picture-button take-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
               <i class="material-icons">camera</i>
             </span>
+            <button @click="back"><img src="/images/back_arrow.svg" alt="" class="back_arrow" /></button>
         </div>
+
     </div>
 </template>
 
@@ -13,7 +17,10 @@
   export default {
   data () {
       return {
-        mediaStream: null
+        mediaStream: null,
+        captureStatus: false,
+        captureData: {},
+        canvas: {},
       }
     },
    mounted() {
@@ -29,10 +36,22 @@
     capture () {
       const mediaStreamTrack = this.mediaStream.getVideoTracks()[0]
       const imageCapture = new window.ImageCapture(mediaStreamTrack)
+      
+      this.captureStatus = true;
+      this.canvas = this.$refs.canvas;
+      this.canvas.getContext("2d").drawImage(imageCapture, 0, 0, 640, 480);
+      this.captureData = this.canvas.toDataURL("image/png");
+
       return imageCapture.takePhoto().then(blob => {
       console.log(blob);
     })
+  },
+
+  back() {
+    console.log("back clicked");
+    this.$router.go(-1);
   }
+
   },
   destroyed () {
     const tracks = this.mediaStream.getTracks()
